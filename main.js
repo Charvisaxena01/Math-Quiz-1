4429b0b0b1d2eceb75f91fb4a43edb094358130d
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2021-03-11 17:02:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function next_page() {
+    player1 = document.getElementById("user1").value;
+    player2 = document.getElementById("user2").value;
+    localStorage.setItem("user1", player1);
+    localStorage.setItem("user2", player2);
     window.location = "index2.html";
-    naame1 = document.getElementById
+
 }
